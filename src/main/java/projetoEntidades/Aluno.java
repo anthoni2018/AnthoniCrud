e2296c07576999b55577de83efdbf01ce145c138
anthoni2018 <42140550+anthoni2018@users.noreply.github.com>
2018-10-27 15:37:17 -0300

    Update Aluno.java

--- src/main/java/projetoEntidades/Aluno.java
+++ src/main/java/projetoEntidades/Aluno.java
@@ -1,65 +1,71 @@
 package projetoEntidades;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+import Crud.entites.Identificavel;
 
 @Entity
-public class Aluno {
+public class Aluno implements Identificavel{
 	
 	@Id
-	@Column(name = "id_alu")
-	private Integer idAluno;
-
-	private int idade;
-	private String nome;
-	private Integer disciplina;
-	private Professor professor;
-	private String turma;
+	@GeneratedValue(generator = "aluno_seq", strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name = "aluno_seq")
 	
-	
-	public void setProfessor(Professor professor) {
-		this.professor = professor;
-	}
-	public Professor getProfessor() {
-		return professor;
-	}
-	
-	public String getTurma() {
-		return turma;
-	}
-	public void setTurma(String turma) {
-		this.turma = turma;
-	}
-	
-	public Integer getDisciplina() {
-		return disciplina;
-	}
-	public void setDisciplina(Integer disciplina) {
-		this.disciplina = disciplina;
-	}
-	public Integer getIdAluno() {
-		return idAluno;
-	}
-	public void setIdAluno(Integer idAluno) {
-		this.idAluno = idAluno;
-	}
-	public int getIdade() {
-		return idade;
-	}
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+	private Long id;
 	@Override
 	public String toString() {
-		return "Aluno [idAluno=" + idAluno + ", idade=" + idade + ", nome=" + nome + ", disciplina=" + disciplina
-				+ ", professor=" + professor + ", turma=" + turma + "]";
+		return "Aluno [id=" + id + ", nomeAluno=" + nomeAluno + ", matricula=" + matricula + ", cpf=" + cpf
+				+ ", nascimento=" + nascimento + "]";
+	}
+	public Aluno() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	private String nomeAluno;
+	private String matricula;
+	private String cpf;
+	private String nascimento;
+	public Long getId() {
+		return id;
 	}
 	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getNomeAluno() {
+		return nomeAluno;
+	}
+	
+	public void setNomeAluno(String nomeAluno) {
+		this.nomeAluno = nomeAluno;
+	}
+	
+	public String getMatricula() {
+		return matricula;
+	}
+	
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public String getNascimento() {
+		return nascimento;
+	}
+	
+	public void setNascimento(String nascimento) {
+		this.nascimento = nascimento;
+	}
 }
